bc95a42d225a688789ce1bfc1d86cc1c625437a5
hyunmi <hymisn@gmail.com>
2023-01-03 17:06:45 +0900

    gg

---
+++ springBootProject_dev/src/main/java/com/springboot/project/hyun/testHyun.java
@@ -0,0 +1,14 @@
+package com.springboot.project.hyun;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class testHyun
+{
+
+	public static void main(String[] args) {
+		System.out.println("hello~~~");
+
+	}
+
+}
