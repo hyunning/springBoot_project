8952e99dc8da5b94c2ec56deff3777d87cecfdc7
hyunmi <hymisn@gmail.com>
2023-01-03 15:09:36 +0900

    test commit

---
+++ springBootProject/src/main/java/com/springboot/project/test/hyun/testClass.java
@@ -0,0 +1,30 @@
+package com.springboot.project.test.hyun;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.project.vo.testClassVO;
+
+@RestController
+public class testClass {
+
+	
+	@GetMapping("/info")
+	public String projectInfo() {
+		return "안녕하세요. 이 프로젝트는 일단 임시로 만들어보았습니다.";
+	}
+	
+	@GetMapping("/info02")
+	public Object projectInfo02() {
+		testClassVO tcVo = new testClassVO();
+		tcVo.author = "hyun";
+		//tcVo.author = null;
+		tcVo.projectNm = "test";
+		tcVo.createData = new Date();
+		
+		return tcVo;
+	}
+
+}
