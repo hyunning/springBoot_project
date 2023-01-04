12779c3b9bd72fd7e6f89c2b2a73ad8c0762223f
hyunmi <hymisn@gmail.com>
2023-01-04 17:00:34 +0900

    bootStrap 적용

---
+++ springBootProject_dev/src/main/java/com/springboot/project/SpringBootProjectDev1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectDev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectDev1Application.class, args);
+	}
+
+}
---
+++ springBootProject_dev/src/main/java/com/springboot/project/restcon/ReturnUiController.java
@@ -0,0 +1,23 @@
+package com.springboot.project.restcon;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ReturnUiController {
+
+	@RequestMapping("/")
+	public Object returnUrlInfo() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("view", "이래도 한글이 깨질거야????");
+		mv.addObject("time", new Date());
+		mv.setViewName("viewTest");
+			
+		return mv;
+		
+	}
+}
